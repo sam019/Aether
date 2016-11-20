bbf794328fb34763a7cf558e127e509884e2069e
梁森 <liangsen2139@foxmail.com>
2016-11-20 13:40:59 +0800

    change backgroud

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -1,23 +1,13 @@
 import React, { PureComponent, PropTypes } from 'react';
-import './App.css';
+import Styles from './App.css';
 
 export default class App extends PureComponent {
   componentDidMount() {
     this.props.startListening();
   }
   render() {
-    const style = {
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontFamily: 'Helvetica Neue, Helvetica, Hiragino Sans GB, Microsoft YaHei, 微软雅黑, Arial, sans-serif',
-      backgroundImage: 'url(https://ooo.0o0.ooo/2016/11/04/581c7cdbe6f64.jpg)',
-      backgroundSize: 'cover',
-    };
     return (
-      <div style={style}>
+      <div className={Styles.wrap}>
         {this.props.children}
       </div>
     );
