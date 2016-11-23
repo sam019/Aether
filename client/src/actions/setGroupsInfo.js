e5df019134361dc8cd8169308d50efa4a8d9e42e
梁森 <liangsen2139@foxmail.com>
2016-11-23 21:04:33 +0800

    fix: enhance setGroupsInfo action

--- client/src/actions/setGroupsInfo.js
+++ client/src/actions/setGroupsInfo.js
@@ -1,6 +1,8 @@
 export default function setGroupsInfo(groups) {
-  for (const group of groups) {
-    group.messages.reverse();
+  if (groups) {
+    for (const group of groups) {
+      group.messages.reverse();
+    }
   }
   return {
     type: 'SET_GROUPS_INFO',
